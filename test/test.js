3874bdb5faa42cb6a563520fd3e410b3c4c20a22
OleksandrDomin <92186960+OleksandrDomin@users.noreply.github.com>
2023-11-19 16:14:09 +0100

    16.30

--- test/test.js
+++ test/test.js
@@ -7,10 +7,8 @@
 
 // const arguments = ("1") => {
 
-  
 //     console.log(arguments)
 // }; vopros
-
 
 // function dec(){
 //     var car = "BMW";
@@ -24,10 +22,26 @@
 // dec()
 // console.log(car)
 
+// const a = { name: "Anton", age: 20 }
+// function dec(b = a) {
+//     console.log(b)
+// }
+// dec();
+
+// -------------------------
+const animals = [
+  { kind: "Cow", order: 3, food: 100, speed: 10 },
+  { kind: "Dog", order: 2, food: 20, speed: 100 },
+  { kind: "Cat", order: 1, food: 3, speed: 150 },
+];
+
+function filterAnimals(filterFunction, animals) {
+    return animals
+        .filter(filterFunction)
+        .sort((animal1, animal2) => animal1.order < animal2.order ? -1 : 1);
+};
+
+const funcFilter = ({ speed }) => speed > 90;
+console.log(filterAnimals(funcFilter, animals));
 
 
-const a = { name: "Anton", age: 20 }
-function dec(b = a) {
-    console.log(b)
-}
-dec();
